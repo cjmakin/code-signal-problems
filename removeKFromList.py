f8ecc3151b06ccf410e1f17b11e9f8edb7fd8149
Christian Makin <christian.makin@gmail.com>
2022-12-06 20:32:35 -0800

    completed removeKFromList

--- removeKFromList.py
+++ removeKFromList.py
@@ -1,11 +1,11 @@
-# Note: Try to solve this task in O(n) time using O(1) additional space, 
+# Note: Try to solve this task in O(n) time using O(1) additional space,
 # where n is the number of elements in the list, since this is what you'll be asked to do during an interview.
-# 
-# Given a singly linked list of integers l and an integer k, remove 
+#
+# Given a singly linked list of integers l and an integer k, remove
 # all elements from list l that have a value equal to k.
 
 class LinkedNode(object):
-    def __init__(self, x) :
+    def __init__(self, x):
         self.value = x
         self.next = None
 
@@ -19,40 +19,31 @@ class LinkedNode(object):
             node = node.next
 
     def remove(self, x):
+        
+        if self.value == x:
+            self = self.next
+            
         node = self.next
         prev = self
-
-        if self.value == x:
-            self = node.next
-            
-        else:
-
-            while node:
-                if node.value == x:
-                    prev.next = node.next
-                prev = node
-                node = node.next
-            
-
+        node.printList()
+        prev.printList()
+        
+        while node:
+            if node.value == x:
+                prev.next = node.next
+            prev = node
+            node = node.next
 
     def printList(self):
         node = self
         while node:
             print(node.value, end=" ")
             node = node.next
-
-    
-        
-
+        print()
 
 list = LinkedNode(1)
-for i in range(2, 20):
+for i in range(2, 21):
     list.add(i)
 
+list.remove(20)
 list.printList()
-list.remove(1)
-print()
-list.printList()
-
-
-    
